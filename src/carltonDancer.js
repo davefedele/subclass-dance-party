9151b1fee5a3b7648364a13235120ceba952678d
David Fedele <dave.fedele@gmail.com>
2014-12-16 20:14:28 -0800

    meeting basic requirements

--- src/carltonDancer.js
+++ src/carltonDancer.js
@@ -1,6 +1,6 @@
 var makeCarltonDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node.append("<img src='images/carlton.gif'>");
+  this.$node.append("<img class='carlton' src='images/carlton.gif'>");
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 };
--- src/init.js
+++ src/init.js
@@ -33,13 +33,41 @@ $(document).ready(function(){
     window.dancers.push(dancer);
   });
 
-$(".lineupDancerButton").on("click", function(event){
-    var count = 1;
-    window.dancers.forEach(function(dancer){
-      dancer.lineup(25,(50 * count * 3));
-      console.log("lining up", dancer);
-      count++;
+  $(".lineupDancerButton").on("click", function(event){
+      var count = 1;
+      window.dancers.forEach(function(dancer){
+        dancer.lineup(25,(50 * count * 3));
+        console.log("lining up", dancer);
+        count++;
+      });
     });
+
+  $("body").on("mouseover", "span", function(event){ 
+    //loop over each dancer
+    var targetDancer = event.target;
+    var distance = {};
+    var that = this;
+    // var smallest = 0;
+      window.dancers.map(function(dancer){
+        var y2 = dancer.$node.position().top;
+        // console.log(dancer);
+        var x2 = dancer.$node.position().left;
+        var y1 = $(that).position().top;
+        var x1 = $(that).position().left;
+        var neighborDistance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        distance[neighborDistance] = dancer;
+        console.log(distance);
+        // console.log('the target:  ', $(this).position());
+        console.log(dancer.$node.position());
+      });
+      for (var key in distance) {
+        if(key < 250){
+          distance[key].$node.addClass('neighbor');
+        }
+      }
+    // console.log($(this).position());
+    
+    // alert(dancer);
   });
 });
 
